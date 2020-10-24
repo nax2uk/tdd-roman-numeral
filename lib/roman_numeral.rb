37d86a0ffa05f785c256d586db48d985dd74e126
nax2uk <azlinayeo@gmail.com>
2020-10-24 17:11:20 +0100

    moved methods around

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -6,23 +6,6 @@ class RomanNumeral
   
     end
 
-    def value_at_position(number, position)
-        (number % (10**position)) / (10**(position-1))
-    end
-
-    def convert_helper(place_value, first_symbol, middle_symbol, last_symbol)
-        case place_value
-        when 0..3
-            return first_symbol * place_value 
-        when 4
-            return first_symbol << middle_symbol
-        when 5..8
-            return middle_symbol << first_symbol*(place_value - 5)
-        when 9
-            return first_symbol << last_symbol
-        end
-    end
-
     def recursive_convert(num, place_value_position)
 
         roman_numeral = {
@@ -45,5 +28,23 @@ class RomanNumeral
             output_string = recursive_convert(num, place_value_position+1) + convert_helper(current_place_value, roman_numeral.values[current_hash_index], roman_numeral.values[current_hash_index+ 1], roman_numeral.values[current_hash_index + 2])
         end
         output_string
+    end
+
+
+    def convert_helper(place_value, first_symbol, middle_symbol, last_symbol)
+        case place_value
+        when 0..3
+            return first_symbol * place_value 
+        when 4
+            return first_symbol << middle_symbol
+        when 5..8
+            return middle_symbol << first_symbol*(place_value - 5)
+        when 9
+            return first_symbol << last_symbol
+        end
+    end
+
+    def value_at_position(number, position)
+        (number % (10**position)) / (10**(position-1))
     end
 end
